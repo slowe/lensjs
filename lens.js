5044cfb6d44e9a847a8439aef941f22372210cbf
Stuart <gravatar@strudel.org.uk>
2013-02-06 15:34:00 +0000

    Modifying the inputs and main functions

--- lens.js
+++ lens.js
@@ -5,37 +5,72 @@
  * Licensed under the MPL http://www.mozilla.org/MPL/MPL-1.1.txt
  *
  */
-
 function Lens(input){
-
     // INPUTS:
     //    width       calculation (canvas) grid width in pixels
     //    height      calculation (canvas) grid height in pixels
     //    pixscale    pixel scale arcsec per pixel: this is used to 
     //                   convert angular coordinates and distances to pixels    
 
-	// Process any input parameters
-	this.w = (input && typeof input.width=="number") ? input.width : 0;
-	this.h = (input && typeof input.height=="number") ? input.height : 0;
-	this.pixscale = (input && typeof input.pixscale=="number") ? input.pixscale : 0;
+    // Set some defaults in case of no input...
 
-	// An array of lens components
-	this.lens = [];
+    this.w = 0;
 
-	// Let's put a source in the centre
-	this.source = { x: parseInt(this.w/2), y: parseInt(this.h/2) };
+    this.h = 0;
 
+    this.pixscale = 1.0;
 
-	// Create 1D arrays
-	// 1) array to hold the predicted image
-	this.predictedimage = new Array(this.w*this.h);
-	// 2) array to hold alpha at each (x,y)
-	this.alpha = new Array(this.w*this.h);
+    // An array of lens components:
+    this.lens = [];
+    // An array of source components:
+    this.source = [];
+    
+    // Some working arrays:
+    this.predictedimage = [];
+    this.alpha = []
+      
+      // Sanity check the input. We must get a width, a height and a pixscale (arcseconds/pixel)
 
+    if(!input) return this;
 
-	return this; // Return the Lens
+    if(input.width && typeof input.width!=="number") return this;
+
+    if(input.height && typeof input.height!=="number") return this;
+
+    if(input.pixscale && typeof input.pixscale!=="number") return this;
+
+    // Process any input parameters
+    this.w = input.width;
+    this.h = input.height;
+    this.pixscale = input.pixscale;
+    // Create 1D arrays
+    // 1) array to hold the predicted image
+    this.predictedimage = new Array(this.w*this.h);
+    // 2) array to hold alpha at each (x,y)
+    this.alpha = new Array(this.w*this.h);
+    return this; // Return the Lens
 }
+Lens.prototype.add = function(component){
+    
+// Input is an object containing:
+//   plane - e.g. 'lens' or 'source'
+//   x and y positions (arcsec relative to centre of grid)
+//  lenses only: theta_e (SIS model, in arcsec)
+//  sources only: size (Gaussian sigma, in arcsec)
+    
+    // Check inputs... inputs are in arcseconds
 
+    if(!component) return this;
+
+    if(!component.plane || typeof component.planet!=="string") return this;
+
+    if(component.plane != "lens" && !component.plane != "source") return this;
+
+    if(component.plane == "lens"){
+
+    if(!component.x || !component.y || !component.theta_e) return this;
+
+<<<<<<< HEAD
 Lens.prototype.addLensComponent = function(component){
 	
     // Check component is sensible... TBD!
@@ -46,66 +81,73 @@ Lens.prototype.addLensComponent = function(component){
 	
 	return this; // Allow this function to be chainable
 }
+=======
+    if(typeof component.x!=="number" || typeof component.y!=="number" || typeof component.theta_e!=="number") return this;
+>>>>>>> Modifying the inputs and main functions
 
-// Lens.prototype.setScale = function(s){
-//	this.scale = s;
-//
-//	return this; // Allow this function to be chainable
-//}
+    }else if (component.plane == "source"){
 
+    if(!component.x || !component.y || !component.size) return this;
+
+    if(typeof component.x!=="number" || typeof component.y!=="number" || typeof component.size!=="number") return this;
+
+    }
+
+    
+    // Rescale angular coordinates and distances to pixels
+    component.x = Math.round(component.x / this.pixscale + this.w/2)
+    component.y = Math.round(component.y / this.pixscale + this.h/2)
+    if(component.plane == "lens") component.theta_e = Math.round(component.theta_e / this.pixscale)
+    if(component.plane == "source") component.size = Math.round(component.size / this.pixscale)
+    
+    if(component.plane == "lens") this.lens.push(component);
+    if(component.plane == "source") this.source.push(component);
+    
+    return this; // Allow this function to be chainable
+}
 // This function will populate this.alpha
 Lens.prototype.calculateAlpha = function(){
-
-	// Set array to zero initially
-	for(var i = 0 ; i < this.w*this.h ; i++){
-		this.alpha[i] = { x: 0.0, y: 0.0 };
-	}
-
-	// Declare outside the for loop for efficiency
-	var x, y, r, cosphi, sinphi;
-
-	// Loop over lens components
-	for(var j = 0 ; j < this.lens.length ; j++){
-	    // Loop over grid pixels
-		for(var i = 0 ; i < this.w*this.h ; i++){
-			x = i % this.w - this.lens[j].x;
-			y = Math.floor(i/this.w) - this.lens[j].y;
-			r = Math.sqrt(x*x+y*y);
-			cosphi = x/r;
-			sinphi = y/r;
-			
-			// Add on contribution from this component
-			this.alpha[i].x += (this.lens[j].theta_e/this.pixscale)*cosphi;
-			this.alpha[i].y += (this.lens[j].theta_e/this.pixscale)*sinphi;
-		}
-	}
-
-	return this; // Allow this function to be chainable
+    // Set array to zero initially
+    for(var i = 0 ; i < this.w*this.h ; i++){
+        this.alpha[i] = { x: 0.0, y: 0.0 };
+    }
+    // Declare outside the for loop for efficiency
+    var x, y, r, cosphi, sinphi;
+    // Loop over lens components
+    for(var j = 0 ; j < this.lens.length ; j++){
+        for(var i = 0 ; i < this.w*this.h ; i++){
+            x = i % this.w - this.lens[j].x;
+            y = Math.floor(i/this.w) - this.lens[j].y;
+            r = Math.sqrt(x*x+y*y);
+            cosphi = x/r;
+            sinphi = y/r;
+            
+            // Add on contribution from this component
+            this.alpha[i].x += this.lens[j].theta_e*cosphi;
+            this.alpha[i].y += this.lens[j].theta_e*sinphi;
+        }
+    }
+    return this; // Allow this function to be chainable
 }
-
 // This function will populate this.predictedimage
 Lens.prototype.calculateImage = function(){
-
-	// Define some variables outside of the loop
-	// as declaring them is expensive
-	var delta = { x: 0, y: 0 };
-	var i = 0;
-	var r2 = 0;
-	var row, col;
-
-	// Loop over x and y. Store 1-D pixel index as i.
-	for(row = 0 ; row < this.h ; row++){
-		for(col = 0 ; col < this.w ; col++){
-			delta.x = col - this.source.x - this.alpha[i].x;
-			delta.y = row - this.source.y - this.alpha[i].y;
-
-			r2 = ( delta.x*delta.x + delta.y*delta.y );
-			this.predictedimage[i] = Math.exp(-r2/50.0);
+    // Define some variables outside of the loop
+    // as declaring them is expensive
+    var delta = { x: 0, y: 0 };
+    var i = 0;
+    var r2 = 0;
+    var row, col;
+    // Loop over x and y. Store 1-D pixel index as i.
+    for(row = 0 ; row < this.h ; row++){
+        for(col = 0 ; col < this.w ; col++){
+            delta.x = col - this.source[0].x - this.alpha[i].x;
+            delta.y = row - this.source[0].y - this.alpha[i].y;
+            r2 = ( delta.x*delta.x + delta.y*delta.y );
+            this.predictedimage[i] = Math.exp(-r2/50.0);
                   // MAGIC number sigma=5 pixels, unlensed source radius...
-
-			i++;
-		}
-	}
-	
-	return this; // Allow this function to be chainable
+            i++;
+        }
+    }
+    
+    return this; // Allow this function to be chainable
 }
