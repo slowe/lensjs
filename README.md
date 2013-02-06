31274233fa64d5837191eb494d1e093303a6da0f
Stuart <gravatar@strudel.org.uk>
2013-02-06 12:04:36 +0000

    Adding lens.js library derived from LensToy.

--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-lensjs
-======
+Lens js
+-------
 
-A Gravitational Lensing library
+A gravitational lensing library in Javascript. See also [LensToy](http://slowe.github.com/LensToy/).
+
+
+Credits
+-------
+
+* Phil Marshall ([University of Oxford](http://www2.physics.ox.ac.uk/research/astrophysics))
+* Stuart Lowe ([LCOGT](http://lcogt.net/))
+* Aprajita Verma ([University of Oxford](http://www2.physics.ox.ac.uk/research/astrophysics))
---
+++ lens.js
@@ -0,0 +1,98 @@
+/*
+ * Javascript Gravitational Lensing Library
+ * 2013 Stuart Lowe (http://lcogt.net/), Phil Marshall (University of Oxford)
+ *
+ * Licensed under the MPL http://www.mozilla.org/MPL/MPL-1.1.txt
+ *
+ */
+
+function Lens(input){
+
+	// Process any input parameters
+	this.w = (input && typeof input.width=="number") ? input.width : 0;
+	this.h = (input && typeof input.height=="number") ? input.height : 0;
+
+	this.scale = 1;
+	this.setScale(4);
+
+	// An array of lens components
+	this.lens = [
+		{theta_e: 10.0, x: parseInt(this.w/2)+ 0.0*this.scale, y: parseInt(this.h/2)- 0.0*this.scale},
+		{theta_e:  3.0, x: parseInt(this.w/2)- 7.0*this.scale, y: parseInt(this.h/2)-27.0*this.scale},
+		{theta_e:  3.0, x: parseInt(this.w/2)+37.0*this.scale, y: parseInt(this.h/2)+37.0*this.scale},
+		{theta_e:  3.0, x: parseInt(this.w/2)+17.0*this.scale, y: parseInt(this.h/2)+52.0*this.scale},
+	];
+
+	// Let's put a source in the centre
+	this.source = { x: parseInt(this.w/2), y: parseInt(this.h/2) };
+
+
+	// Create 1D arrays
+	// 1) array to hold the predicted image
+	this.predictedimage = new Array(this.w*this.h);
+	// 2) array to hold alpha at each (x,y)
+	this.alpha = new Array(this.w*this.h);
+
+
+	return this; // Return the Lens
+}
+
+Lens.prototype.setScale = function(s){
+	this.scale = s;
+}
+
+// This function will populate this.alpha
+Lens.prototype.calculateAlpha = function(){
+
+	// Set array to zero initially
+	for(var i = 0 ; i < this.w*this.h ; i++){
+		this.alpha[i] = { x: 0.0, y: 0.0 };
+	}
+
+	// Declare outside the for loop for efficiency
+	var x, y, r, cosphi, sinphi;
+
+	// Loop over lens components
+	for(var j = 0 ; j < this.lens.length ; j++){
+		for(var i = 0 ; i < this.w*this.h ; i++){
+			x = i % this.w - this.lens[j].x;
+			y = Math.floor(i/this.w) - this.lens[j].y;
+			r = Math.sqrt(x*x+y*y);
+			cosphi = x/r;
+			sinphi = y/r;
+			
+			// Add on contribution from this component
+			this.alpha[i].x += this.lens[j].theta_e*this.scale*cosphi;
+			this.alpha[i].y += this.lens[j].theta_e*this.scale*sinphi;
+		}
+	}
+
+	return this; // Allow this function to be chainable
+}
+
+// This function will populate this.predictedimage
+Lens.prototype.calculateImage = function(){
+
+	// Define some variables outside of the loop
+	// as declaring them is expensive
+	var delta = { x: 0, y: 0 };
+	var i = 0;
+	var r2 = 0;
+	var row, col;
+
+	// Loop over x and y. Store 1-D pixel index as i.
+	for(row = 0 ; row < this.h ; row++){
+		for(col = 0 ; col < this.w ; col++){
+			delta.x = col - this.source.x - this.alpha[i].x;
+			delta.y = row - this.source.y - this.alpha[i].y;
+
+			r2 = ( delta.x*delta.x + delta.y*delta.y );
+			this.predictedimage[i] = Math.exp(-r2/50.0);
+                  // MAGIC number sigma=5 pixels, unlensed source radius...
+
+			i++;
+		}
+	}
+	
+	return this; // Allow this function to be chainable
+}
